af8ac210658ac5080023752aa6c8dab008c945d4
R0Rsh3ck <R0Rsh3ck@gmail.com>
2017-04-10 23:10:39 +0200

    Create login.php

---
+++ login.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title></title>
+	</head>
+	<body>
+		<center>
+			<form action="process.php" method="POST">
+				<h1>Login Form</h1>
+				<hr />
+				username: <input type="text" name="username" /><br />
+				password: <input type="password" name="password" /><br />
+				<input type="submit" value="login" name="login" style="width:70px;"/>
+			</form>
+		</center>
+	</body>
+</html>
